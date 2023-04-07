a87683d47a03ff97dce51979b7c6f98c76693435
Mohit Prashant Patil <70277626+patilMohitDD@users.noreply.github.com>
2023-04-06 20:02:44 -0700

    Madlib Game (#6)

---
+++ Madlibs Game/constants.py
@@ -0,0 +1,24 @@
+
+
+
+madlib_sentences = [f"The {adj_1} stood on the edge of the mountain,\
+staring out at the {adj_2} landscape. It had always dreamed of {verb_1} to the other side, but had never had the courage to try.\
+Finally, the {noun_1} took a deep breath and {verb_2} forward, determined to make it to the other side.\
+It was a long and difficult journey, but the {noun_2} never gave up, and in the end, it emerged triumphant.",
+
+f"The {adj_1} was a popular spot for tourists, who came from all over the world to {verb_1} its beautiful {noun_1}. \
+The {adj_2} was known for its crystal-clear waters and {noun_2} beaches. Visitors could spend hours {verb_2} on the sand, \
+soaking up the sun and enjoying the peaceful sound of the waves.",
+
+f"The {adj_1} was a fearsome creature, \
+feared by all who knew of its existence. It was said to {verb_1} its prey with lightning-{verb_2} speed, \
+leaving no chance for escape. The {adj_2} was most often found in the depths of the {noun_1}, \
+where it {noun_2} for its next victim.",
+
+f"The {adj_1} was a sight to behold, with its towering {noun_1} and {adj_2} walls. \
+It was said to be impenetrable, and many had tried and failed to {verb_1} it over the years. \
+But one day, a brave group of adventurers decided to take on the challenge. \
+They {verb_2} across the moat and scaled the walls, finally reaching the top and claiming victory. \
+The {noun_2} had been conquered at last."
+
+]
