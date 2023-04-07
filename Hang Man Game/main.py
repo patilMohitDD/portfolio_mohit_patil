c45002d89cf98585caa542ec13776e7f9dc6b806
Mohit Prashant Patil <70277626+patilMohitDD@users.noreply.github.com>
2023-04-06 22:36:02 -0700

    Hang man (#7)
    * Hangman function completed
    
    * Output File added

---
+++ Hang Man Game/main.py
@@ -0,0 +1,4 @@
+from functions import hangman_Logic
+
+hangman_Logic()
+
---
+++ Hang Man Game/words_Data.py
@@ -0,0 +1,16 @@
+words_List = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', \
+'honeydew', 'imbe', 'jackfruit', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'peach', \
+'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli fruit', 'vanilla bean', 'watermelon', \
+'xylocarp', 'yellow watermelon', 'zucchini', 'almond', 'brazil nut', 'cashew', 'date nut', \
+'elderberry', 'filbert', 'grapefruit', 'hazelnut', 'italian chestnut', 'jujube', 'kola nut', \
+'lemon', 'macadamia nut', 'navel orange', 'olive', 'pistachio', 'quince', 'raisin', 'strawberry', \
+'tangerine', 'ugli fruit', 'vanilla bean', 'walnut', 'xylocarp', 'yellow watermelon', 'zucchini', \
+'albatross', 'beaver', 'chimpanzee', 'dingo', 'elephant', 'flamingo', 'gorilla', 'hippopotamus', 'iguana', \
+'jaguar', 'koala', 'lemur', 'manatee', 'narwhal', 'opossum', 'panda', 'quokka', 'raccoon', 'sloth', 'toucan', \
+'uakari', 'vulture', 'walrus', 'xerus', 'yak', 'zebra', 'airplane', 'balloon', 'car', 'drive', 'elevator', \
+'ferry', 'glider', 'helicopter', 'inline skate', 'jeep', 'kayak', 'limousine', 'motorcycle', 'navy', \
+'outrigger canoe', 'parachute', 'quadracycle', 'racecar', 'sailboat', 'train', 'unicycle', 'van', 'wheelbarrow', \
+'xylophone', 'yacht', 'zeppelin', 'astronomy', 'biology', 'chemistry', 'dendrology', 'ethology', 'forensic science', \
+'genetics', 'histology', 'ichthyology', 'junk science', 'kinesiology', 'limnology', 'meteorology', 'neuroscience', \
+'oceanography', 'psychology', 'quantum mechanics', 'radiology', 'sociology', 'toxicology', 'urology', \
+'volcanology', 'whirlpool', 'xenophobia', 'yoga', 'zoology'] 
