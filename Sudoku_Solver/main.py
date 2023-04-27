09a399a49b605dcfd0f6e96066a19377a06ca15e
patilMohitDD <70277626+patilMohitDD@users.noreply.github.com>
2023-04-26 23:07:14 -0700

    Summary
    - Created API end point get_sudoku_by_id

--- Sudoku_Solver/main.py
+++ Sudoku_Solver/main.py
@@ -3,12 +3,19 @@ from typing import Optional
 from store import sudokus
 from fastapi.responses import JSONResponse
 import json
+from pprint import pprint
 
 app = FastAPI()
 
-@app.get("/get_sudoku_to_solve")
+@app.get("/get_sudokus")
 async def get_sudoku(id:Optional[int] = None , description="This will a give all the Sudokus we have to solve with there ID's"):
+    return json.dumps(sudokus)
+    # response = JSONResponse(content=sudokus) # beautfiying the response and sending
+    # return response
 
-    # content = json.dumps(sudokus)
-    response = JSONResponse(content=sudokus) # beautfiying the response and sending
-    return response
+@app.get(("/get_sudoku_by_id/{id}"))
+async def get_sudoku_by_id(id:int = Path(gt=0)): # id should be greater than 0
+    response = sudokus.get(id)
+    return {id: response}
+
+
